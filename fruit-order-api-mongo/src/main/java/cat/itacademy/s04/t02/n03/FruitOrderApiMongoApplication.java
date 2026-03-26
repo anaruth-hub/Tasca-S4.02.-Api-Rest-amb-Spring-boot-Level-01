0c5423f061478a02b71ce6ecbdbf407ee622022b
Ana Ruth <aniarinna@gmail.com>
2026-03-26 11:36:01 +0100

    feat: add  mongo application entry point

---
+++ fruit-order-api-mongo/src/main/java/cat/itacademy/s04/t02/n03/FruitOrderApiMongoApplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t02.n03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitOrderApiMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitOrderApiMongoApplication.class, args);
+    }
+}
